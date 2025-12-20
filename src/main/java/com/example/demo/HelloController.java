62ec40f2badc7765dcf9ea97b98d309a7e62bb4c
Bayastan <bayastan.ishembaev@netigate.se>
2025-12-21 00:09:32 +0100

    feat: add name param

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,7 +1,9 @@
 package com.example.demo;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -13,7 +15,8 @@ public class HelloController {
     }
 
     @GetMapping("/greeting")
-    public String greeting() {
-        return "greeting"; //
+    public String greeting(@RequestParam(defaultValue = "FreeStylo") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
     }
 }
